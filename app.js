fc42eca828e57823208407034431ed9ab31214ac
parthselarka <116080750+parthselarka@users.noreply.github.com>
2024-11-07 15:50:19 +0530

    api key changes

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const port = 3000;
 require('dotenv').config();
 
 
-const api_key = process.env.API_KEY;
+const api_key = "ed760929087b5cc543d905d586d4740c";
 
 // Predefined list of genres with their corresponding IDs from TMDB
 const genres = [
